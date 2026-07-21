fcb8d118ce2253050f93f3cb1d35b958c6e29300
anyi11 <anyi11@yahoo.com>
2026-07-21 21:42:38 +0800

    Update d.bat

--- d.bat
+++ d.bat
@@ -1,2 +1,17 @@
-assoc .exe=exefile
-ftype exefile="%1" %*
+@echo off
+echo 正在修复 .exe 文件关联...
+
+:: 删除用户自定义的错误关联
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.exe\UserChoice" /f
+
+:: 恢复系统默认关联
+cmd /c assoc .exe=exefile
+cmd /c ftype exefile="%%1" %%*
+
+:: 重启资源管理器以生效
+echo 正在重启资源管理器...
+taskkill /f /im explorer.exe
+start explorer.exe
+
+echo 修复完成！请尝试打开任意软件。
+pause
